2c95dc838a89f0a7912145a4120bdc1b67273256
Petar Nenov <petar_nenov@abv.bg>
2019-01-31 21:10:04 +0200

    --amend

--- routes/updateUserInfo.js
+++ routes/updateUserInfo.js
@@ -6,24 +6,25 @@ const client = require("../client/client.js");
 
 router.patch("/", function(request, response, next) {
   const id = request.body.id;
-  const first_name = request.body.first_name;
-  const last_name = request.body.last_name;
-  const email = request.body.email;
-
-  client.hmset(
-    id,
-    "first_name",
-    first_name,
-    "last_name",
-    last_name,
-    "email",
-    email,
-    (err, obj) => {
-      const result = JSON.stringify({ value: err ? false : true });
-      response.setHeader("Content-Type", "application/json");
-      response.send(result);
-    }
-  );
+  const obj = client.hgetall(id, (err, obj) => {
+    const first_name = request.body.first_name || obj.first_name;
+    const last_name = request.body.last_name || obj.last_name;
+    const email = request.body.email || obj.email;
+    client.hmset(
+      id,
+      "first_name",
+      first_name,
+      "last_name",
+      last_name,
+      "email",
+      email,
+      (err, obj) => {
+        const result = JSON.stringify({ value: err ? false : true });
+        response.setHeader("Content-Type", "application/json");
+        response.send(result);
+      }
+    );
+  });
 });
 
 module.exports = router;
